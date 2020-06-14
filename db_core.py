a169a38ab321e3acd090711c645cbbf7c2aa8528
sab1e <antony.sobolev@gmail.com>
2020-06-14 19:28:44 +0500

    add mapper

--- db_core.py
+++ db_core.py
@@ -2,7 +2,6 @@ import threading
 import sqlite3
 
 from models import Employee
-
 connection = sqlite3.connect('projects_data.sqlite')
 
 
@@ -70,6 +69,84 @@ class EmployeeMapper:
             raise DbDeleteException(e.args)
 
 
-employee_mapper = EmployeeMapper(connection)
-employee_1 = employee_mapper.find_by_id(2)
-print(employee_1.__dict__)
+class MapperRegistry:
+    @staticmethod
+    def get_mapper(obj):
+        if isinstance(obj, Employee):
+            return EmployeeMapper(connection)
+
+
+class UnitOfWork:
+    current = threading.local()
+
+    def __init__(self):
+        self.new_objects = []
+        self.dirty_objects = []
+        self.removed_objects = []
+
+    def register_new(self, obj):
+        self.new_objects.append(obj)
+
+    def register_dirty(self, obj):
+        self.dirty_objects.append(obj)
+
+    def register_removed(self, obj):
+        self.removed_objects.append(obj)
+
+    def commit(self):
+        self.insert_new()
+        self.update_dirty()
+        self.delete_removed()
+
+    def insert_new(self):
+        for obj in self.new_objects:
+            MapperRegistry.get_mapper(obj).insert(obj)
+
+    def update_dirty(self):
+        for obj in self.dirty_objects:
+            MapperRegistry.get_mapper(obj).update(obj)
+
+    def delete_removed(self):
+        for obj in self.removed_objects:
+            MapperRegistry.get_mapper(obj).delete(obj)
+
+    @staticmethod
+    def new_current():
+        __class__.set_current(UnitOfWork())
+
+    @classmethod
+    def set_current(cls, unit_of_work):
+        cls.current.unit_of_work = unit_of_work
+
+    @classmethod
+    def get_current(cls):
+        return cls.current.unit_of_work
+
+
+class DomainObject:
+
+    def mark_new(self):
+        UnitOfWork.get_current().register_new(self)
+
+    def mark_dirty(self):
+        UnitOfWork.get_current().register_dirty(self)
+
+    def mark_removed(self):
+        UnitOfWork.get_current().register_removed(self)
+
+
+#
+# class Category(DomainObject):
+#     def __init__(self, name):
+#         self.name = name
+#
+#
+# class CategoryMapper:
+#     pass
+
+
+# employee_mapper = EmployeeMapper(connection)
+# employee_1 = employee_mapper.find_by_id(2)
+# print(employee_1.__dict__)
+
+
